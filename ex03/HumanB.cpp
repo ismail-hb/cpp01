03f3ef9f9c540d25c9a3eb9cec70b6b19e554af9
ismail-hb <ismail.houche@icloud.com>
2025-03-05 10:50:45 +0100

    jshqd

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   HumanB.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ismail <ismail@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 18:14:01 by ismail            #+#    #+#             */
-/*   Updated: 2025/03/03 20:30:39 by ishouche         ###   ########.fr       */
+/*   Updated: 2025/03/04 07:51:27 by ismail           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 
 void HumanB::attack()
 {
-	std::cout << name << " attacks with their " << type << std::endl;
+	if (type)
+		std::cout << name << " attacks with their " << type->getType() << std::endl;
+	else
+		std::cout << name << " has no weapon" << std::endl;
 }
 
 void HumanB::setWeapon(Weapon& type)
